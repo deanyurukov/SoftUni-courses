642c3b8cc3a12d68700373be172970df7408143a
Dean Yurukov <dean.yurukov2008@gmail.com>
2026-04-13 14:10:04 +0300

    11.Exam-Preparation-2-Part-5

--- TypeScript February 2026/12.Exam-Preparation-2/dist/models.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ContentType = void 0;
-var ContentType;
-(function (ContentType) {
-    ContentType[ContentType["Series"] = 0] = "Series";
-    ContentType[ContentType["Movie"] = 1] = "Movie";
-    ContentType[ContentType["Documentary"] = 2] = "Documentary";
-})(ContentType || (exports.ContentType = ContentType = {}));
